fb7ffce463da7b35b924e4d3a0977a80dced3d82
TomasCatalan <tcatalaota@educacion.navarra.es>
2024-01-25 20:51:51 +0100

    Update Ejercicio1.py

--- Ejercicio1.py
+++ Ejercicio1.py
@@ -14,17 +14,35 @@ class Producto:
         self.__codigo = valor
     
     @property
-    def codigo(self):
-        return self.__codigo
+    def nombre(self):
+        return self.__nombre
     
-    @codigo.setter
-    def codigo(self, valor):
-        self.__codigo = valor    
+    @nombre.setter
+    def nombre(self, valor):
+        self.__nombre = valor    
     
-@property
-    def codigo(self):
-        return self.__codigo
+    @property
+    def precio(self):
+        return self.__precio
     
-    @codigo.setter
-    def codigo(self, valor):
-        self.__codigo = valor
+    @precio.setter
+    def precio(self, valor):
+        self.__precio = valor
+
+    def __str__(self):
+        return "Codigo: " + str(self.__codigo) + ", nombre: " + self.__nombre + ", precio: " + str(self.__codigo)
+    
+    def calcular_total(self, unidades):
+        return self.precio * unidades
+
+p1 = Producto(1, "Producto 1", 5)
+p2 = Producto(2, "Producto 2", 10)
+p3 = Producto(3, "Producto 3", 20)
+
+print(p1)
+print(p2)
+print(p3)
+
+print(p1.calcular_total(5))
+print(p2.calcular_total(5))
+print(p3.calcular_total(5))
